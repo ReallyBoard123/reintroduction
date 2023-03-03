53445f85bb8e0ccb7c8af025a2ab77d656cf0870
Chirag Singal <chirag.prakriya@gmail.com>
2023-03-03 10:09:36 +0100

    this is going to be fun >.<

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 const untracked = "I am untracked";
+
+const trouble = "I am trouble";
+const muhahaha = "I am evil";
+
+function makeTrouble() {
+	console.log(trouble);
+}
+
+function makeMuhahaha() {
+	console.log(muhahaha);
+}
